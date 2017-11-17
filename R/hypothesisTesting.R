3000decd348061a95fdb7ddaf24fedc018c3982d
david.seb.fischer <david.fischer@helmholtz-muenchen.de>
2017-11-16 16:30:05 -0800

    Finished lrt

--- R/hypothesisTesting.R
+++ R/hypothesisTesting.R
@@ -3,55 +3,33 @@
 #' @note Must be run after running fit.differential
 #' TODO: adatapt this to only perform test and not fit reduced model
 #'
-#' @param obj the MpraObject containing the full model
+#' @param obj the MpraObject containing the full and reduced
 #' @param dnaDesign.reduced the design of the DNA reduced model. If NULL, DNA is
 #' assumed to follow same model as the full model - and raDesign.reduced must be
 #' provided 
 #' @param rnaDesign.reduced the design of the RNA reduced model. If NULL, RNA is
 #' assumed to follow same model as the full model, and dnaDesign.reduced must be
 #' provided
-test.lrt <- function(obj, dnaDesign.reduced=NULL, rnaDesign.reduced) {
-    ## TODO: check that mode is 'LRT', or just that it's not 'quant'?
-    if (obj@model != "LRT") {
-        stop("fit.differential must be run in 'LRT' mode prior to calling this function")
-    } else if (is.null(dnaDesign.reduced) & is.null(rnaDesign.reduced)) {
-        stop("DNA and RNA can't both be the same as the full model")
-    } else if (is.null(dnaDesign.reduced)) {
-        dnaDesign.reduced <- obj@designs@dnaFull
-    } else if (is.null(rnaDesign.reduced)) {
-        rnaDesign.reduced <- obj@designs@rnaFull
-    }
-
-    obj@designs@dnaRed <- getDesignMat(obj, dnaDesign.reduced)
-    obj@designs@rnaRed <- getDesignMat(obj, rnaDesign.reduced)
-
-    ##TODO: make sure the reduced model is nested within the full model?
-
-    obj@ModelFits.reduced <- bplapply(rownames(obj@dnaCounts), function(rn) {
-        return(fitfun(dcounts=obj@dnaCounts[rn,],
-                      rcounts=obj@rnaCounts[rn,],
-                      ddepth=obj@dnaDepth,
-                      rdepth=obj@rnaDepth,
-                      ddesign.mat=obj@designs@dnaRed,
-                      rdesign.mat=obj@designs@rnaRed,
-                      compute.hessian=compHess))
-    }, BPPARAM = obj@BPPARAM)
-
-    ll.full <- unlist(lapply(obj@modelFits,
-                             function(x) x$ll), use.names = TRUE)
-    ll.red <- unlist(lapply(obj@modelFits.reduced,
-                            function(x) x$ll), use.names = TRUE)
-    res <- merge(data.frame('full'=ll.full),
-                 data.frame('reduced'=ll.red), by = 0, all=FALSE)
-    colnames(res)[1] <- "Feature" ##TODO: what do we call these?
-
-    res$lrt.statistic <- 2*(res$full - res$reduced)
-
-    df.full <- NCOL(obj@designs@dnaFull) + NCOL(obj@designs@rnaFull)
-    df.red <- NCOL(obj@designs@dnaRed) + NCOL(obj@designs@rnaRed)
-    res$p.val <- pchisq(res$lrt.statistic, df = df.full-df.red)
-    res$fdr <- p.adjust(res$p.val, 'BH')
-
-    obj@results <- res
-    return(obj)
+#' 
+#' @return results data frame
+test.lrt <- function(obj) {
+    
+    ll.full <- sapply(obj@modelFits, function(x) x$ll)
+    ll.red <- sapply(obj@modelFits.red, function(x) x$ll)
+    df.full <- sapply(obj@modelFits, function(x) x$d.df + x$r.df + x$r.ctrl.df )
+    df.red <- sapply(obj@modelFits.red, function(x) x$d.df + x$r.df + x$r.ctrl.df )
+    #df.full <- NCOL(obj@designs@dnaFull) + NCOL(obj@designs@rnaFull)
+    #df.red <- NCOL(obj@designs@dnaRed) + NCOL(obj@designs@rnaRed)
+    res <- data.frame(enhancer=rownames(obj@dnaCounts),
+                      ll.full=ll.full,
+                      ll.red=ll.red,
+                      df.full=df.full,
+                      df.red=df.red,
+                      row.names=rownames(obj@dnaCounts))
+    
+    res$lrt.statistic <- 2*(res$ll.full - res$ll.red)
+    res$pval <- pchisq(res$lrt.statistic, df = res$df.full-res$df.red)
+    res$fdr.pval <- p.adjust(res$pval, 'BH')
+    
+    return(res)
 }
