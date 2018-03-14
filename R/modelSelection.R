96f2c1050bf9686394a631fd8489bad69e89548a
david.seb.fischer <david.fischer@helmholtz-muenchen.de>
2018-03-14 15:16:13 +0100

    moved integer test on rna obsrvations with NB likelihood into setModel and added warning and automated rounding if violated

--- R/modelSelection.R
+++ R/modelSelection.R
@@ -32,6 +32,18 @@ setModel <- function(obj, model) {
     } else {
         stop("model '", model, "' not supported.")
     }
+    
+    # check that data is integer if count distribution is used
+    # round if this is not the case and issue a warning
+    if(obj@model %in% c("ln.nb", "gamma.pois")){
+        if(!all(obj@rnaCounts%%1 == 0)) {
+            warning("Negative binomial count distribution of rna model in ",
+                    obj@model, " requires integer RNA observations. ",
+                    "Rounded RNA count matrix to integers.")
+            obj@rnaCounts <- round(obj@rnaCounts)
+        }
+    }
+    
     if(model == "ln.nb") {
         obj@dnaCounts[obj@dnaCounts==0] <- PSEUDOCOUNT
     }
