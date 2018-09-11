393f418a820a0b39ad9206cbdd572514ef8c701b
talashuach <tal.ashuach@gmail.com>
2018-09-10 18:49:54 -0700

    data documentation update

--- R/data.R
+++ R/data.R
@@ -1,28 +1,44 @@
-#'Column annotations for the chromosomal-episomal dataset
+#' Sample MPRA data
+#' 
+#' A subset of MPRA data from Inoue et al., comparing enhancer activity of
+#' episomal constructs vs. chromosomally integrated constructs (integration
+#' was performed with a lentivirus). Data included negative control enhancers,
+#' multiple batches and barcodes, a subsample of which are included in this
+#' sample data for runtime purposes.
+#'
+#'@docType data
+#'
+#'@usage data(ChrEpi)
+#'
 #'@format
+#'\describe{
+#'\item{ce.colAnnot}{
+#'Column annotations for each column (sample) in the data matrices
 #'\describe{
 #'\item{batch}{batch identifier, factor}
 #'\item{condition}{condition identifier, factor. WT corresponds to chromosomal
 #'and MT corresponds to episomal}
 #'\item{barcode}{barcode identifier, factor}
 #'}
+#'}
+#'\item{ce.dnaCounts}{DNA observations}
+#'\item{ce.rnaCounts}{DNA observations}
+#'\item{ce.control}{indices of control enhancers}
+#'}
+#'
 #'@source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5204343/}
-"colAnnot"
+#'
+#'@name ChrEpi
+NULL
 
-#' DNA observations for the chromosomal-episomal dataset
-#' @format integer matrix
-#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5204343/}
-"dnaCounts"
+#'@name ChrEpi
+"ce.colAnnot"
 
-#' DNA observations for the chromosomal-episomal dataset
-#' @format integer matrix
-#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5204343/}
-"rnaCounts"
+#'@name ChrEpi
+"ce.dnaCounts"
 
-#' indices of control enhancers in the chromosomal-episomal dataset
-#' @format logical vector, TRUE means the corresponding row in the observation
-#' matrices are control features
-#' @source #' DNA observations for the chromosomal-episomal dataset
-#' @format integer matrix
-#' @source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5204343/}
-"control"
+#'@name ChrEpi
+"ce.rnaCounts"
+
+#'@rdname ChrEpi
+"ce.control"
